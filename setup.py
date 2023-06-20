cab5ff370c9c73300b1c0b7461c4e64032c587e0
Saeid Naderiparizi <saeidnp@cs.ubc.ca>
2023-06-20 14:58:21 -0700

    Fix setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,12 @@ from setuptools import find_packages
 setup(
     name='synthetic-datasets',
     version='0.0.1',
-    py_modules=["synthetic_datasets"],
+    packages=["synthetic_datasets"],
     license='MIT License',
     install_requires=[
-            'sklearn',
+            'scikit-learn',
             'matplotlib',
             'numpy',
             'common_utils @ git+ssh://git@github.com/saeidnp/common-utils.git@master#egg=common_utils',
-            # 'torch>=1.0.1',
-            # 'torchvision>=0.2.2'
         ],
 )
